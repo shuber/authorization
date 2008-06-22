60c96fdf553eee37cd1faa556bd1e943f3a02a14
Sean Huber <sean.huber@cox.net>
2008-06-22 02:18:36 -0700

    Protected the controller instance methods

--- lib/huberry/authorization/controller_methods.rb
+++ lib/huberry/authorization/controller_methods.rb
@@ -16,30 +16,31 @@ module Huberry
 				def authorize(options = {})
 					before_filter_keys = [:only, :except]
 					before_filter_options = options.delete_if { |key, value| before_filter_keys.include? key }
-				  before_filter(before_filter_options) { |controller| controller.authorize options }
+				  before_filter(before_filter_options) { |controller| controller.send :authorize, options }
 				end
 			end
 			
 			module InstanceMethods
-				def authorize(options = {})
-					unauthorized unless authorized?(options)
-				end
-				
-				def authorized?(options = {})
-					rights = (options.has_key? :right ? [options[:right]] : options[:rights]) || []
-					roles = (options.has_key? :role ? [options:role] : options[:roles]) || []
-					if !send(:current_user).nil? && send(:current_user).has_rights?(rights) && send(:current_user).is_roles?(roles)
-						block_given? ? yield : true
-					else
-						block_given? ? nil : false
+				protected
+					def authorize(options = {})
+						unauthorized unless authorized?(options)
 					end
-				end
 				
-				def unauthorized
-					flash[:error] = self.class.unauthorized_message
-					redirect_to respond_to?(self.class.unauthorized_redirect_path) ? send(self.class.unauthorized_redirect_path) : self.class.unauthorized_redirect_path.to_s
-					false
-				end
+					def authorized?(options = {})
+						rights = (options.has_key? :right ? [options[:right]] : options[:rights]) || []
+						roles = (options.has_key? :role ? [options:role] : options[:roles]) || []
+						if !send(:current_user).nil? && send(:current_user).has_rights?(rights) && send(:current_user).is_roles?(roles)
+							block_given? ? yield : true
+						else
+							block_given? ? nil : false
+						end
+					end
+				
+					def unauthorized
+						flash[:error] = self.class.unauthorized_message
+						redirect_to respond_to?(self.class.unauthorized_redirect_path) ? send(self.class.unauthorized_redirect_path) : self.class.unauthorized_redirect_path.to_s
+						false
+					end
 			end
 		end
 	end
